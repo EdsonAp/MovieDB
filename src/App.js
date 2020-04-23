6130d923b3ed5e98055da6c94188bdca59068c87
Edson Aparicio <edson.aparicio@icloud.com>
2020-04-22 22:48:23 -0500

    refactored state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,76 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
-import { 
-  BrowserRouter as Router, 
-  Route,
-  Switch,
-  Link, } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import MoviesList from "./MoviesList";
 import MovieDetail from "./MovieDetail";
 
-const App = (props) =>(
+const URL ="https://api.themoviedb.org/3/discover/movie?api_key=72fb3457014533b9c7a1d7d1bb1a9e3f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=";
+const Page = 1;
+
+export default class App extends Component {
+  state = {
+    movies: [],
+    pageNumber: Page,
+    loading: true
+  };
+
+  componentDidMount() {
+    const { pageNumber } = this.state;
+    this.fetchMoreMovies(pageNumber);
+  }
+
+  fetchMoreMovies = async pg => {
+    const { movies } = this.state;
+    fetch(`${URL}${pg}`)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          movies: [...movies, ...res.results]
+        });
+      })
+      .catch(error => this.setState({ loading: false }));
+  };
+
+  updatePageNumber = pageNumber => {
+    this.setState({ pageNumber });
+    this.fetchMoreMovies(pageNumber);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
       <Router>
         <div className="App">
           <header className="App-header">
-            <Link to="/" >
-              <img src={logo} className="App-logo" alt="logo" />
-            </Link>
+          <Link className='Link' to="/">
+            {'EDSON\'S MOVIE DATABASE'}
+          </Link>
           </header>
-          <Switch> 
-            <Route exact path='/' render={props => (<MoviesList {...props} />)}/>
-            <Route path='/:id' component={MovieDetail}/>
+          <Switch>
+            <Route exact path='/' render={props => (<MoviesList {...props} state={this.state} fetchMoreMovies={this.fetchMoreMovies} updatePageNumber={this.updatePageNumber} />)}/>
+            <Route exact path='/:id' component={MovieDetail}/>
           </Switch>
         </div>
-        
       </Router>
+      </React.Fragment>
+
     );
+  }
+}
 
+// const App = (props) =>(
+//       <Router>
+//         <div className="App">
+//           <header className="App-header">
+//             <Link to="/" >
+//               <img src={logo} className="App-logo" alt="logo" />
+//             </Link>
+//           </header>
+//           <Switch>
+//             <Route exact path='/' render={props => (<MoviesList {...props}/>)}/>
+//             <Route path='/:id' component={MovieDetail}/>
+//           </Switch>
+//         </div>
 
-export default App;
+//       </Router>
+//     );
--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,60 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 import Movie from "./Movie";
 
 
-class MoviesList extends Component {
-  state = {
-    movies: [],
-    pageNumber: 1,
-  };
-
-  
-  clickHandler = (pageNumber) => {
-    this.setState({pageNumber});
-    this.fetchMoreMovies(pageNumber);
-  };
-
-  fetchMoreMovies = async (page) => {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=72fb3457014533b9c7a1d7d1bb1a9e3f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
-    );
-    const movies = await res.json();
-    this.setState({
-      movies: [...this.state.movies, ...movies.results]
-    });
-    console.log(this.state.movies);
-
+const MoviesList = ({
+  updatePageNumber,
+  state: {
+    movies,
+    pageNumber,
   }
- 
-  componentDidMount() {
-    try {
-      const pageNumber = this.state;
-      this.fetchMoreMovies(pageNumber);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  render() {
-    //destructuring
-    const { pageNumber, movies } = this.state;
-    return (
-      <div>
-        <MovieGrid>{movies.map(movie => 
-          (<Movie key={movie.id} movie={movie}/>))}
-        </MovieGrid>
-        <button onClick={() => this.clickHandler(pageNumber + 1)}>MORE MOVIES</button>
-      </div>
-    );
-  }
-}
+}) => (
+  <React.Fragment>
+    <MovieGrid>
+      {movies.map(movie=> <Movie key={movies.id} movie={movie} />)}
+    </MovieGrid>
+    <Button type='button' onClick={() =>updatePageNumber(pageNumber + 1)}>MORE MOVIES</Button>
+  </React.Fragment>
+)
 
 export default MoviesList;
 
 const MovieGrid = styled.div`
   display: grid;
   padding: 1rem;
-  grid-template-columns: repeat(6, 1fr);
+  grid-template-columns: repeat(5, 1fr);
   grid-row-gap: 1rem;
+`;
+
+const Button = styled.button`
+  display: flex;
+  background: white;
+  margin: 2rem auto;
+  height: 3rem;
+  box-shadow: 0 0 35px white;
+  background: black;
+  color: white;
+  justify-content: center;
+  width: 150px;
+  cursor: pointer;
+  outline: none;
 `;
